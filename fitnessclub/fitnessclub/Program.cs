f59bd6790150a2b0307086bbafdcb929f9e1fb83
James W. Tonkin <btonkin71@comcast.net>
2017-07-10 16:16:45 -0500

    FInal work from chapter 8.

--- fitnessclub/fitnessclub/Program.cs
+++ fitnessclub/fitnessclub/Program.cs
@@ -31,13 +31,60 @@ namespace fitnessclub
             memberID = pMemberID;
             memberSince = pMemberSince;
         }
+
+        public virtual void CalculateAnnualFee()
+        {
+            annualFee = 0;
+        }
     }
 
+    class NormalMember : Member
+    {
+        public NormalMember(string remarks, string name, int memberID, int memberSince) : base (name, memberID, memberSince)
+        {
+            Console.WriteLine("Child Constructor with 4 parameters");
+            Console.WriteLine("Remarks = {0}", remarks);
+        }
+
+        public override void CalculateAnnualFee()
+        {
+            annualFee = 100 + 12 * 30;
+        }
+    }
+
+    class VIPMember : Member
+    {
+        public VIPMember(string name, int memberID, int memberSince) : base (name, memberID, memberSince)
+        {
+            Console.WriteLine("Child Constructor with 3 parameters");
+        }
+
+        public override void CalculateAnnualFee()
+        {
+            annualFee = 1200;
+        }
+    }
 
     class Program
     {
         static void Main(string[] args)
         {
+            Member[] clubMembers = new Member[5];
+
+            clubMembers[0] = new
+            NormalMember("Special Rate", "James", 1, 2010);
+            clubMembers[1] = new
+            NormalMember("Normal Rate", "Andy", 2, 2011);
+            clubMembers[2] = new
+            NormalMember("Normal Rate", "Bill", 3, 2011);
+            clubMembers[3] = new VIPMember("Carol", 4, 2012);
+            clubMembers[4] = new VIPMember("Evelyn", 5, 2012);
+
+            foreach (Member m in clubMembers)
+            {
+                m.CalculateAnnualFee();
+                Console.WriteLine(m.ToString());
+            }
         }
     }
 }

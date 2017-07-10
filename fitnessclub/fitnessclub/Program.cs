a3c1a3ba1b6f242594128589821e7e7061b2148d
James W. Tonkin <btonkin71@comcast.net>
2017-07-10 15:18:39 -0500

    Initial Commit

---
+++ fitnessclub/fitnessclub/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fitnessclub
+{
+    class Member
+    {
+        protected int annualFee;
+        private string name;
+        private int memberID;
+        private int memberSince;
+
+        public override string ToString()
+        {
+            return "\nName: " + name + "\nMember ID: " + memberID + "\nMember Since: " + memberSince + "\nTotal Annual Fee: " + annualFee;
+        }
+
+        public Member()
+        {
+            Console.WriteLine("Parent Constructo with no parameter");
+        }
+
+        public Member(string pName, int pMemberID, int pMemberSince)
+        {
+            Console.WriteLine("Parent Constructor with 3 parameters");
+
+            name = pName;
+            memberID = pMemberID;
+            memberSince = pMemberSince;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
